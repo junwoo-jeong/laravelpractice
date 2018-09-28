8d9415af8a395828af1b3141a3efb2b449d37f1d
smallThinking <wnsdnek778@gmail.com>
2018-09-28 13:45:04 +0900

    데이터베이스 마이그레이션

---
+++ database/migrations/2018_09_27_114331_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    
+    public function up() // 마이그레이션을 실행하는 메서드
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() // 롤백을 실행하는 메서드
+    {
+        Schema::dropIfExists('posts');
+    }
+}
